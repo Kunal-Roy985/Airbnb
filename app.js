59594b138234e8b1af9f6525c3467b28e391faf2
Kunal Roy <kunalr985y@gmail.com>
2025-05-23 13:21:45 +0530

    Add Initial Files

--- app.js
+++ app.js
@@ -114,7 +114,7 @@ app.use("/listings/:id/reviews", reviewRouter);
 app.use("/" , userRouter)
 
 app.all("*", (req, res, next) => {
-  next(new ExpressError(404, "Page not Found!"));
+  res.redirect("/listings")
 });
 
 //error handler--------------->
